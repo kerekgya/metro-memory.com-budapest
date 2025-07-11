1654d732d6982b5ed0394bec60e9ba4794962bde
Benjamin Tran Dinh <benjamin.tdm@gmail.com>
2025-07-11 16:18:48 +0200

    Ensure main fills viewport

--- src/app/(game)/chicago/page.tsx
+++ src/app/(game)/chicago/page.tsx
@@ -22,7 +22,7 @@ export const metadata = config.METADATA
 export default function Chicago() {
   return (
     <Provider value={config}>
-      <main className={font.className}>
+      <main className={`${font.className} min-h-screen`}>
         <GamePage fc={fc} />
       </main>
     </Provider>
--- src/app/(game)/wien/page.tsx
+++ src/app/(game)/wien/page.tsx
@@ -23,7 +23,7 @@ export const metadata = config.METADATA
 export default function Wien() {
   return (
     <Provider value={config}>
-      <main className={font.className}>
+      <main className={`${font.className} min-h-screen`}>
         <GamePage fc={fc} />
       </main>
     </Provider>
